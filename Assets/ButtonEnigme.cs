d40c4409cdb4aabea550deb001cfb5bb1b917f60
Kiramuun <deimonlight@live.fr>
2024-01-12 18:36:12 +0100

    Update

--- Assets/ButtonEnigme.cs
+++ Assets/ButtonEnigme.cs
@@ -6,7 +6,7 @@ public class ButtonEnigme : MonoBehaviour
 {
     public Animator[] _animButton = new Animator[4];
     Animator _animDLaby;
-    int _index = 0;
+    int _index;
 
     void Awake()
     {
@@ -15,6 +15,31 @@ public class ButtonEnigme : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        
+        if (_animButton[2].isActiveAndEnabled)
+        {
+            _index = 1;
+        }
+        if (_animButton[1].isActiveAndEnabled || _animButton[0].isActiveAndEnabled || _animButton[3].isActiveAndEnabled)
+        {
+            //_index = 0;
+        }
+
+        switch (_index)
+        {
+            case 0:
+                _animButton[0].SetTrigger("ErrorButton1");
+                _animButton[1].SetTrigger("ErrorButton1");
+                _animButton[2].SetTrigger("ErrorButton1");
+                _animButton[3].SetTrigger("ErrorButton1");
+                break;
+
+            case 1:
+                if (_animButton[2].isActiveAndEnabled)
+                {
+                    if (_animButton[0].isActiveAndEnabled) { _index = 2; }
+                    else if(_animButton[1].isActiveAndEnabled || _animButton[3].isActiveAndEnabled) { _index = 0; }
+                }
+                break;
+        }
     }
 }
